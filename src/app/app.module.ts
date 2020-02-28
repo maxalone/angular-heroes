1c82cf947e3b1bd32447887e5429f20142b0672a
maxalone <mascam64@gmail.com>
2020-02-28 18:01:26 +0100

    3 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HeroesComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
